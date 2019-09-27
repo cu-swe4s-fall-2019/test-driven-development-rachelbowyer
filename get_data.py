7928ee7b1297cb596b4ba65f99ac850815d47177
Rachel Bowyer <rabo8409@colorado.edu>
2019-09-27 04:18:33 +0000

    fixed error in get_data.py

--- get_data.py
+++ get_data.py
@@ -10,7 +10,7 @@ def read_stdin_col(col_num):
         A = l.rstrip().split()
         X.append(float(A[0]))
         Y.append(float(A[1]))
-    XY_arr = np.zeros((len(X), len(A)))  # convert to array
+    XY_arr = np.zeros((len(X), 2))  # convert to array
     XY_arr[:, 0] = X
     XY_arr[:, 1] = Y
     return XY_arr[:, col_num]
