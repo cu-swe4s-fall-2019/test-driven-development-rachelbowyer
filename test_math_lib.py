df00c4a0510f8c6b09934949fbac64ef2a089f58
Rachel Bowyer <rabo8409@colorado.edu>
2019-09-27 00:56:01 +0000

    test for mean of constant list

--- test_math_lib.py
+++ test_math_lib.py
@@ -10,14 +10,20 @@ class TestMathLib(unittest.TestCase):
     def test_list_mean_none(self):
         """test for if list_mean is passed None"""
         r = math_lib.list_mean(None)
-        self.assertEqual(r,None)
+        self.assertEqual(r, None)
 
 
     def test_list_mean_empty_list(self):
         """test for if list_mean is passed empty list"""
         r = math_lib.list_mean([])
-        self.assertEqual(r,None)
-        
-        
+        self.assertEqual(r, None)
+
+
+    def test_list_mean_constants(self):
+        """test for mean of constant list"""
+        r = math_lib.list_mean([1, 1, 1, 1])
+        self.assertEqual(r, 1)
+
+
 if __name__ == '__main__':
     unittest.main()
