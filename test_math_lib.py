58bd4594439fed2368ce5c4b8e96026a149af4a6
Rachel Bowyer <rabo8409@colorado.edu>
2019-09-27 01:02:38 +0000

    test_math_lib.py conforms to pep8 style now

--- test_math_lib.py
+++ test_math_lib.py
@@ -12,47 +12,43 @@ class TestMathLib(unittest.TestCase):
         r = math_lib.list_mean(None)
         self.assertEqual(r, None)
 
-
     def test_list_mean_empty_list(self):
         """test for if list_mean is passed empty list"""
         r = math_lib.list_mean([])
         self.assertEqual(r, None)
-
 
     def test_list_mean_constants(self):
         """test for mean of constant list"""
         r = math_lib.list_mean([1, 1, 1, 1])
         self.assertEqual(r, 1)
 
-
     def test_list_mean_rand_ints(self):
         """test for mean of int list"""
         L = []
         for i in range(100):
             for j in range(10):
-                L.append(random.randint(0,100))
+                L.append(random.randint(0, 100))
             r = math_lib.list_mean(L)
             e = stat.mean(L)
-            self.assertEqual(r,e)
+            self.assertEqual(r, e)
 
-   
     def test_list_mean_rand_floats(self):
         """test for mean of float list"""
         L = []
         for i in range(100):
             for j in range(10):
-                L.append(random.uniform(0,100))
+                L.append(random.uniform(0, 100))
             r = math_lib.list_mean(L)
             e = stat.mean(L)
-            self.assertTrue(m.isclose(r,e))
-
+            self.assertTrue(m.isclose(r, e))
 
     def test_list_mean_non_real_number_in_list(self):
+        """test if list has bad inputs"""
         L = []
         for i in range(10):
-            L.append(random.randint(0,100))
+            L.append(random.randint(0, 100))
         L.append('x')
-        
+
         with self.assertRaises(ValueError) as ex:
             math_lib.list_mean(L)
 
